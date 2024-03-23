f5812b1c2b7b7a49a2af6fbaee881efccc8119c8
zzinao <kjn0265@gmail.com>
2024-03-24 00:25:04 +0900

    feat: hit-counter 추가

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -4,6 +4,9 @@ import './style.scss';
 function PageFooter({ author, githubUrl }) {
   return (
     <footer className="page-footer-wrapper">
+      <a href="https://hits.seeyoufarm.com">
+        <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fzzinao.github.io&count_bg=%2379C83D&title_bg=%237A7A7A&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
+      </a>
       <p className="page-footer">
         © {new Date().getFullYear()}
         &nbsp;
